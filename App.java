db63ea25295e3ea3674227d894498b1d5aa44e88
Hessne <109930983+Hessne@users.noreply.github.com>
2022-07-24 22:32:07 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,41 @@
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+
+        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
+        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+
+        // String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        String url = "https://api.mocki.io/v2/549a5d8b/MostPopularTVs";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+        // exibir e manipular os dados 
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
+        var geradora = new GeradoraDeFigurinhas();
+
+        for (int i = 0; i < 3; i++) {
+
+            Conteudo conteudo = conteudos.get(i);
+
+            InputStream inputStream2 = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saidafigura/" + conteudo.getTitulo().replace(":","") + ".png";
+            System.out.println(nomeArquivo);
+            System.out.println();
+            geradora.cria(inputStream2, nomeArquivo);
+
+            System.out.println(conteudo.getTitulo());
+            System.out.println();
+        }
+    }
+}
